17e4c45ae982e48192f348d404b5a9c26c596883
dorao <hiroki.tokonami.09.17@gmail.com>
2022-05-31 23:42:33 +0900

    アニメーション周りを追加

--- docs/assets/js/application.js
+++ docs/assets/js/application.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   hamburger();
+  scrollPage();
+  closeSpNav();
+  scrollTrigger();
 });
 
 function hamburger() {
@@ -12,9 +15,37 @@ function hamburger() {
   if (!(spNav = document.querySelector(".js-nav"))) {
     return;
   }
-
   hamburger.addEventListener("click", () => {
     spNav.classList.toggle("active");
     hamburger.classList.toggle("active");
   });
+}
+
+function scrollPage() {
+  let spNavLinks = document.querySelectorAll("a[data-target]");
+  spNavLinks.forEach(function (element) {
+    element.addEventListener("click", (e) => {
+      // aタグをクリックした際のリンクジャンプをキャンセルする
+      e.preventDefault();
+      // クリックされたナビアイテムのclass名を取得する
+      className = element.getAttribute("data-target");
+      pos = document.querySelector(className).offsetTop - 100;
+      $("html,body").animate({ scrollTop: pos }, 400);
+    });
+  });
+}
+
+function closeSpNav() {
+  let spNavLinks = document.querySelectorAll(".l-header__sp-nav-link");
+  for (var i = 0; i < spNavLinks.length; i++) {
+    spNavLinks[i].addEventListener("click", function () {
+      document.querySelector(".js-nav").classList.remove("active");
+      document.querySelector(".js-hamburger").classList.remove("active");
+    });
+  }
+}
+
+function scrollTrigger() {
+  const trigger = new ScrollTrigger.default();
+  trigger.add("[data-trigger]");
 }
